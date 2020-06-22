ffaa9c9cb3aa89566cfa840fe3e9be875bf07500
Thirapa <po.thirapa_st@tni.ac.th>
2020-06-22 11:05:59 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-module.exports = {
-    hello: function(msg) {
-        return "Hello " + msg;
-    }
-};
+// module.exports = {
+//     hello: function(msg) {
+//         return "Hello " + msg;
+//     }
+// };
+
+module.exports = (name) => `Hello ${name}`;
